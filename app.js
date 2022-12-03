b0300cc452ed6e1e72702925c02829a561734bca
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-03 14:40:23 +0100

    tetris pieces created & drew first rotation in 1st tetris piece

---
+++ app.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid'); 
+    let squares = Array.from(document.querySelectorAll('.grid div')); 
+    const ScoreDisplay = document.querySelector('#score'); 
+    const StartBtn = document.querySelector('#start-button');
+    const width = 10; 
+
+    // The Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2], 
+        [width, width+1, width+2, width*2+2], 
+        [1, width+1, width*2+1, width*2], 
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1], 
+        [width+1, width+2, width*2, width*2+1], 
+        [0, width, width+1, width*2+1], 
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2], 
+        [1, width+1, width+2, width*2+1], 
+        [width, width+1, width+2, width*2+1], 
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1], 
+        [0, 1, width, width+1], 
+        [0, 1, width, width+1], 
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1], 
+        [width, width+1, width+2, width+3], 
+        [1, width+1, width*2+1, width*3+1], 
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    let currentPosition = 4; 
+    let current = theTetrominoes[0][0];
+
+    // draw the first rotation in the first tetromino 
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino');
+        })
+    } 
+
+    draw()
+})
